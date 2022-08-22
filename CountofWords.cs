d32d195d3e24b152ced583e73441bcc040bef5e3
Shishir Alva S <53567077+Shishiralva20@users.noreply.github.com>
2022-08-22 16:53:48 +0530

    Add files via upload

---
+++ CountofWords.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDemo
+{
+    internal class CountofWords
+    {
+        public static void Main()
+        {
+            int a = 0, myWord = 1;
+            string str = "Digital Engineer Trainee in Sonata Software Limited";
+            while (a <= str.Length - 1)
+            {
+                if (str[a] == ' ' || str[a] == '\n' || str[a] == '\t')
+                {
+                    myWord++;
+                }
+                a++;
+            }
+            Console.Write("Number of words in the string = {0}\n", myWord);
+        }
+    }
+}
