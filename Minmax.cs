d1ee3d902f1ef69f31ea62ccba3856633fdfed31
Shishir Alva S <53567077+Shishiralva20@users.noreply.github.com>
2022-08-25 00:03:51 +0530

    Add files via upload

---
+++ Minmax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Minmax
+    {
+        public static void Main()
+        {
+            int[] arr = { 20, 24, 6, 45, 3, 98 };
+            int i, max, min, n;
+            n = arr.Length;
+            max = arr[0];
+            min = arr[0];
+            for (i = 1; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.Write("Maximum number = {0}\n", max);
+            Console.Write("Minimum number = {0}\n", min);
+        }
+    }
+}
---
+++ Negativenumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Negativenumbers
+    {
+        public static void Main()
+        {
+            int[] arr = { -12, -34, 3, -67, -97, 30 };
+            int i, n;
+            n = arr.Length;
+            for (i = 0; i < n; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.Write($"{arr[i]}");
+                }
+
+            }
+        }
+    }
+}
